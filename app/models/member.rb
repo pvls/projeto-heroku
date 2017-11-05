82f037166a813400632cbce63f86c11ce392c37e
eacj <eacj@cin.ufpe.br>
2017-11-05 15:57:33 +0000

    modificacoes

--- app/models/member.rb
+++ app/models/member.rb
@@ -8,6 +8,17 @@ class Member < ApplicationRecord
       member.email = auth.info.email
       member.name = auth.info.name
       member.password = Devise.friendly_token[0,20]
+      member.image = auth.info.image
     end
+  end
+  
+  def largeimage
+    "http://graph.facebook.com/#{self.uid}/picture?type=large"
+  end
+  def normalimage
+    "http://graph.facebook.com/#{self.uid}/picture?type=normal"
+  end
+  def smallimage
+    "http://graph.facebook.com/#{self.uid}/picture?type=small" 
   end
 end
